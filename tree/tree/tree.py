d9896b6c217046e8cca2b6bba68b5dcb44cca4f3
Ayat <alkayedayat93@gmail.com>
2022-04-15 06:26:42 +0300

    CC 15" "

--- tree/tree/tree.py
+++ tree/tree/tree.py
@@ -134,23 +134,22 @@ class BinaryTree(TNode):
             return my_list
 
     def Max(self):
+        my_max = [self.root.value]
         def _traverse(node):
-                max1 = self.root.value
-                max2 = self.root.value
                 if node:
+                    max2 = node.value
                     # print(node.value)
-                    if node.value > max1:
-                        max2= node.value
+                    if my_max[0]> max2 :
+                        my_max[0]= my_max[0]
                     else:
-                        max2 = max2
+                        my_max[0] = max2
                     if node.left:
                         _traverse(node.left )
                     if node.right:
                         _traverse(node.right)
-                return max2
-       
+                return my_max[0]
         return _traverse(self.root )
-        
+         
 
 class Binary_search_tree(BinaryTree):
     def __init__(self):
@@ -161,6 +160,8 @@ class Binary_search_tree(BinaryTree):
             return 'empty'
 
         def _contain(node):
+            '''best way to traverse the search tree to search a target value --> pre-order traverse'''
+            '''root --> left -- > right'''
             if node:
                 if node.value == value:
                     return True
@@ -209,40 +210,40 @@ if __name__ == '__main__':
     # node5 = TNode('E')
     # node6=TNode('F')
     # # node7=TNode('G')
-    # # node1 = TNode(1)
-    # # node2 = TNode(2)
-    # # node3 = TNode(3)
-    # # node4 = TNode(4)
-    # # node5 = TNode(5)
-    # # node6=TNode(6)
-    # node1.left = node2
-    # node1.right = node3
-    # node2.left = node4
-    # node2.right= node5
-    # node3.left= node6
-    # # node3.right = node7
+    node1 = TNode(7)
+    node2 = TNode(2)
+    node3 = TNode(3)
+    node4 = TNode(4)
+    node5 = TNode(5)
+    node6=TNode(6)
+    node1.left = node2
+    node1.right = node3
+    node2.left = node4
+    node2.right= node5
+    node3.left= node6
+    # node3.right = node7
     
-    # tree = BinaryTree()
-    # tree.root = node1
+    tree = BinaryTree()
+    tree.root = node1
     # tree.Breadth_first()
     # print(tree.Breadth_first())
-    # print(tree.Max())
+    print(tree.Max())
 
-    binary_search_tree = Binary_search_tree()
-    binary_search_tree.Add(20)
-    binary_search_tree.Add(5)
-    binary_search_tree.Add(30)
-    binary_search_tree.Add(1)
-    binary_search_tree.Add(15)
-    binary_search_tree.Add(9)
-    binary_search_tree.Add(12)
-    binary_search_tree.Add(25)
-    binary_search_tree.Add(40)
-    binary_search_tree.Add(7)
+    # binary_search_tree = Binary_search_tree()
+    # binary_search_tree.Add(20)
+    # binary_search_tree.Add(5)
+    # binary_search_tree.Add(30)
+    # binary_search_tree.Add(1)
+    # binary_search_tree.Add(15)
+    # binary_search_tree.Add(9)
+    # binary_search_tree.Add(12)
+    # binary_search_tree.Add(25)
+    # binary_search_tree.Add(40)
+    # binary_search_tree.Add(7)
 
     # binary_search_tree.Pre_order_rec()
     # print(binary_search_tree.In_order_rec())
-    print(binary_search_tree.Contains(1))
+    # print(binary_search_tree.Contains(1))
     # print(binary_search_tree.root.left.left.value)
     # print(tree.Pre_order_rec())
     # print(tree.In_order_rec())
