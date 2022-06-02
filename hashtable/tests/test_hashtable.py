2046820dae60d0a0d2f03ab661d4b909d4a1b820
Ayat <alkayedayat93@gmail.com>
2022-06-02 15:31:39 +0300

    hash implementation

--- hashtable/tests/test_hashtable.py
+++ hashtable/tests/test_hashtable.py
@@ -1,12 +1,36 @@
+from pyparsing import java_style_comment
 from hashtable.hashtable import Hashtable
+
 
 def test_get():
     hashtable = Hashtable()
     hashtable.set("Django", "python")
     actual = hashtable.get("Django")
-    expected = 17
+    expected = "python"
     assert actual == expected
 
+def test_get2():
+    hashtable = Hashtable()
+    hashtable.set("Django", "python")
+    hashtable.set("ognajD", "nohtyp")
+    actual = hashtable.get("Django")
+    expected = "python"
+    assert actual == expected
+
+def test_get3():
+    hashtable = Hashtable()
+    hashtable.set("Django", "python")
+    hashtable.set("ognajD", "nohtyp")
+    actual = hashtable.get("ognajD")
+    expected = "nohtyp"
+    assert actual == expected
+
+def test_get4():
+    hashtable = Hashtable()
+    hashtable.set("Django", "python")
+    actual = hashtable.get("Djang")
+    expected = None
+    assert actual == expected
 
 def test_contains():
     hashtable = Hashtable()
@@ -17,7 +41,6 @@ def test_contains():
 
 def test_contains2():
     hashtable = Hashtable()
-    hashtable.set("Django", "python")
     actual = hashtable.contains("ayat")
     expected = False
     assert actual == expected
@@ -28,3 +51,12 @@ def test_hash():
     actual = hashtable.hash("ayat")
     expected = 773
     assert actual == expected
+
+def test_delete():
+    hashtable = Hashtable()
+    hashtable.set("Django", "python")
+    hashtable.delete("Django")
+    actual = hashtable.get("Django")
+    expected = None
+    actual == expected
+
